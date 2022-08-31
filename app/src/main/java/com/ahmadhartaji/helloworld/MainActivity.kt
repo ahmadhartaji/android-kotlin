92c0ad7ec41024b8325bb52d9a3e4731ddda060e
ahmadhartaji <ahmadhartaji@gmail.com>
2022-08-31 14:39:00 +0700

    Menambahkan Sanity Check

--- app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ahmadhartaji.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.ahmadhartaji.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,16 +19,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hitung() {
-        val berat = binding.editTextBerat.text.toString().toFloat()
-        val tinggi = binding.editTextTinggi.text.toString().toFloat() / 100
-        val rumusBMI = berat / (tinggi * tinggi)
+        val berat = binding.editTextBerat.text.toString()
+        if (TextUtils.isEmpty(berat)) {
+            Toast.makeText(this, "Berat tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val tinggi = binding.editTextTinggi.text.toString()
+        if (TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(this, "Tinggi tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
 
         val selectedId = binding.radioGroup.checkedRadioButtonId
+        if (selectedId == -1) {
+            Toast.makeText(this, "Kategori tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val tinggiCm = tinggi.toFloat() / 100
+        val rumusBMI = berat.toFloat() / (tinggiCm * tinggiCm)
         val isMale = selectedId == R.id.radioButtonPria
+        val kategori = getKategori(rumusBMI, isMale)
 
         binding.textViewHasil.text = getString(R.string.bmi_x, rumusBMI)
-        binding.textViewKategori.text =
-            getString(R.string.kategori_x, getKategori(rumusBMI, isMale))
+        binding.textViewKategori.text = getString(R.string.kategori_x, kategori)
     }
 
     private fun getKategori(bmi: Float, isMale: Boolean): String {
