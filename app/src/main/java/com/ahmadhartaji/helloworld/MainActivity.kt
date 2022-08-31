e3fbde2d28dd39584531d1a84ca18dd45061fac1
ahmadhartaji <ahmadhartaji@gmail.com>
2022-08-31 14:16:21 +0700

    Menghitung BMI dan Kategori

--- app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ahmadhartaji.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.ahmadhartaji.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +13,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonHitung.setOnClickListener { hitung()  }
+        binding.buttonHitung.setOnClickListener { hitung() }
     }
+
     private fun hitung() {
-        Log.d("MainActivity", "Tombol Berhasil di Klik")
+        val berat = binding.editTextBerat.text.toString().toFloat()
+        val tinggi = binding.editTextTinggi.text.toString().toFloat() / 100
+        val rumusBMI = berat / (tinggi * tinggi)
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.radioButtonPria
+
+        binding.textViewHasil.text = getString(R.string.bmi_x, rumusBMI)
+        binding.textViewKategori.text =
+            getString(R.string.kategori_x, getKategori(rumusBMI, isMale))
+    }
+
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        } else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
