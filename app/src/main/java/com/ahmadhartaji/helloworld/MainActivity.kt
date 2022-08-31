7ca5668ea98c84a82172078e8c716013710b231e
ahmadhartaji <ahmadhartaji@gmail.com>
2022-08-31 10:03:12 +0700

    Menambahkan aksi lanjut

--- app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ahmadhartaji.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -9,21 +10,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var textView: TextView
+    private var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val data = arrayListOf<Hewan>().apply {
-            add(Hewan("Ayam",R.drawable.ayam))
-            add(Hewan("Bebek",R.drawable.bebek))
-            add(Hewan("Domba",R.drawable.domba))
-            add(Hewan("Kambing",R.drawable.kambing))
-            add(Hewan("Sapi",R.drawable.sapi))
+            add(Hewan("Ayam", R.drawable.ayam))
+            add(Hewan("Bebek", R.drawable.bebek))
+            add(Hewan("Domba", R.drawable.domba))
+            add(Hewan("Kambing", R.drawable.kambing))
+            add(Hewan("Sapi", R.drawable.sapi))
         }
         imageView = findViewById(R.id.imageView)
         textView = findViewById(R.id.textView)
-        showData(data[0])
+        showData(data[number])
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+
+            number = (number + 1) % data.size
+            showData(data[number])
+        }
     }
 
     private fun showData(hewan: Hewan) {
