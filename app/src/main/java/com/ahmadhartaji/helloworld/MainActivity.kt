c5f12d1d2ec180695f43fcba7ffea7bf9463766e
ahmadhartaji <ahmadhartaji@gmail.com>
2022-09-01 09:21:20 +0700

    Membuat layout untuk list

--- app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
@@ -2,15 +2,8 @@ package com.ahmadhartaji.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var imageView: ImageView
-    private lateinit var textView: TextView
-    private var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +16,5 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Kambing", R.drawable.kambing))
             add(Hewan("Sapi", R.drawable.sapi))
         }
-        imageView = findViewById(R.id.imageView)
-        textView = findViewById(R.id.textView)
-        showData(data[number])
-
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-
-            number = (number + 1) % data.size
-            showData(data[number])
-        }
-    }
-
-    private fun showData(hewan: Hewan) {
-        imageView.setImageResource(hewan.gambarResId)
-        textView.text = hewan.nama
     }
 }
