4c1ec2033a4b493011ed04615f2743e15e0a89fc
ahmadhartaji <ahmadhartaji@gmail.com>
2022-09-01 11:19:23 +0700

    Memindahkan data ke view model

--- app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainActivity.kt
@@ -2,32 +2,23 @@ package com.ahmadhartaji.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.ahmadhartaji.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recyclerView.adapter = MainAdapter(getData())
+        binding.recyclerView.adapter = MainAdapter(viewModel.data)
         binding.recyclerView.setHasFixedSize(true)
-    }
-    private fun getData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa"   , "Cygnus olor"             , R.drawable.angsa),
-            Hewan("Ayam"    , "Gallus gallus"           , R.drawable.ayam),
-            Hewan("Bebek"   , "Cairina moschata"        , R.drawable.bebek),
-            Hewan("Domba"   , "Ovis ammon"              , R.drawable.domba),
-            Hewan("Kalkun"  , "Meleagris gallopavo"     , R.drawable.kalkun),
-            Hewan("Kambing" , "Capricornis sumatrensis" , R.drawable.kambing),
-            Hewan("Kelinci" , "Oryctolagus cuniculus"   , R.drawable.kelinci),
-            Hewan("Kerbau"  , "Bubalus bubalis"         , R.drawable.kerbau),
-            Hewan("Kuda"    , "Equus caballus"          , R.drawable.kuda),
-            Hewan("Sapi"    , "Bos taurus"              , R.drawable.sapi),
-        )
     }
 }
