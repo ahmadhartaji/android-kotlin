2b0a985c46e4257d39fdc836a77d375481e3f23f
ahmadhartaji <ahmadhartaji@gmail.com>
2022-09-01 14:40:36 +0700

    Memproses data dari internet

--- app/src/main/java/com/ahmadhartaji/helloworld/MainViewModel.kt
+++ app/src/main/java/com/ahmadhartaji/helloworld/MainViewModel.kt
@@ -15,7 +15,6 @@ class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
-        data.value = initData()
         retrieveDataFromServer()
     }
 
@@ -23,7 +22,7 @@ class MainViewModel : ViewModel() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val result = HewanApi.service.getHewan()
-                Log.d("hewan", result)
+                data.postValue(result)
             } catch (e: Exception) {
                 Log.d("hewan", "Gagal : ${e.message}")
             }
@@ -32,18 +31,4 @@ class MainViewModel : ViewModel() {
 
     fun getData(): LiveData<List<Hewan>> = data
 
-    private fun initData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa"   , "Cygnus olor"             , R.drawable.angsa),
-            Hewan("Ayam"    , "Gallus gallus"           , R.drawable.ayam),
-            Hewan("Bebek"   , "Cairina moschata"        , R.drawable.bebek),
-            Hewan("Domba"   , "Ovis ammon"              , R.drawable.domba),
-            Hewan("Kalkun"  , "Meleagris gallopavo"     , R.drawable.kalkun),
-            Hewan("Kambing" , "Capricornis sumatrensis" , R.drawable.kambing),
-            Hewan("Kelinci" , "Oryctolagus cuniculus"   , R.drawable.kelinci),
-            Hewan("Kerbau"  , "Bubalus bubalis"         , R.drawable.kerbau),
-            Hewan("Kuda"    , "Equus caballus"          , R.drawable.kuda),
-            Hewan("Sapi"    , "Bos taurus"              , R.drawable.sapi),
-        )
-    }
 }
